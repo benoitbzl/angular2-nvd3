d668d3ec07e90e942e286c55bfeb7e872409832f
benoitbzl <benoitbzl@users.noreply.github.com>
2017-04-19 11:41:56 +0200

    Update graph on data changes.
    Brutal update of the graph if data changes.

--- src/angular2-nvd3/angular2-nvd3.component.ts
+++ src/angular2-nvd3/angular2-nvd3.component.ts
@@ -1,13 +1,16 @@
 import { Component, ElementRef, Input, OnChanges, OnInit } from '@angular/core';
+import { SimpleChanges, DoCheck } from '@angular/core';
 import { Angular2NvD3 } from './angular2-nvd3.core';
+import * as _ from 'lodash';
 
 @Component({ selector: 'app-nvd3', template: '' })
-export class NvD3Component implements OnChanges, OnInit {
+export class NvD3Component implements OnChanges, OnInit, DoCheck {
     @Input()
     public options: any;
 
     @Input()
     public data: any;
+    private previousData: {key: string, values: any[]}[];
 
     private ngNvD3: any;
 
@@ -15,8 +18,22 @@ export class NvD3Component implements OnChanges, OnInit {
         this.ngNvD3 = new Angular2NvD3.NgNvD3(this.el);
     }
 
-    ngOnChanges(changes?) {
-        this.ngNvD3.updateWithOptions(this.options, this.data);
+    ngDoCheck() {
+        if (!_.isEqual(this.data, this.previousData)) {
+            this.ngNvD3.updateWithOptions(this.options, this.data);
+            this.previousData = _.cloneDeep(this.data);
+        }
+    }
+
+    ngOnChanges(changes?: SimpleChanges) {
+        if ('data' in changes && 'options' in changes) {
+            const data = changes['data'].currentValue;
+            const options = changes['options'].currentValue;
+            if (data && options) {
+                this.ngNvD3.updateWithOptions(this.options, this.data);
+            }
+            this.previousData = _.cloneDeep(this.data);
+        }
     }
 
     ngOnInit() {
